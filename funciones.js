c752eaaf905ec996c348d229c9b0e04244c17a77
María Virginia Cavallero 408492 <408492@sistemas.frc.utn.edu.ar>
2026-03-21 09:30:14 -0300

    docs: actualizar mensaje de bienvenida

--- funciones.js
+++ funciones.js
@@ -2,3 +2,4 @@ console.log("Proyecto iniciado correctamente.");
 console.log("Cambios en mi rama local")
 
 console.log("Proyecto iniciado - version 1.0");
+console.log("Bienvenidos al repositorio del Grupo UTN");
